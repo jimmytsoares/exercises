09071a3a38c46bf9c6519679cc4896f573bc2f2d
Jimmy <jimmytsoares@gmail.com>
2020-07-14 23:47:25 -0300

    First exercise

---
+++ Exercises/LeetCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercises
+{
+    public static class LeetCode
+    {
+        /// <summary>
+        /// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var prev = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (prev.ContainsKey(nums[i]))
+                    return new int[] { prev[nums[i]], i };
+
+                if (!prev.ContainsKey(target - nums[i]))
+                    prev.Add(target - nums[i], i);
+            }
+
+            return new int[2];
+        }
+    }
+}
---
+++ UnitTests/LeetCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LeetCode
+    {
+        [Fact]
+        public void TwoSumBasic()
+        {
+            var ans = Exercises.LeetCode.TwoSum(new int[] { 1, 2, 3, 4 }, 5);
+
+            Assert.Equal(new int[] { 1, 2 }, ans);
+        }
+
+        [Fact]
+        public void TwoSumRepeating()
+        {
+            var ans = Exercises.LeetCode.TwoSum(new int[] { 1, 2, 2, 3, 4 }, 5);
+
+            Assert.Equal(new int[] { 1, 3 }, ans);
+        }
+    }
+}
